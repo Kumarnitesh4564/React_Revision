1d9298d4f72896d396d402230497327a61c99bdc
Nitesh Kumar <kumarnitesh70040@gmail.com>
2026-08-14 19:37:24 +0530

    initial commit

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='card'>
+        <h1>I am Nitesh</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            In enim, ipsa vel velit iure magni perspiciatis praesentium nulla repellat voluptate temporibus 
+            quibusdam ex corporis aliquid quod vero pariatur quos est!
+            Eum aspernatur ab maxime odit nemo? Eius cupiditate nihil aliquam 
+            amet facilis quo consequuntur blanditiis rerum. Eos alias sequi pariatur 
+            officiis dolores? Quasi inventore quod porro recusandae vero deleniti dolores?
+        </p>
+    </div>
+  )
+}
+
+export default Card
---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='nav'>
+        <button>Logo</button>
+        <button>Home</button>
+        <button>Search</button>
+        <button>About</button>
+    </div>
+  )
+}
+
+export default Navbar
